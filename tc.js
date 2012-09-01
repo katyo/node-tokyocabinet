96ee9cfce10eba65386ac7099575172397b98136
Kayo Phoenix <kayo@illumium.org>
2012-09-01 10:32:14 +0600

    Hooks added.. Methods wrapping fixed..

--- tc.js
+++ tc.js
@@ -5,15 +5,18 @@ var reSync = /Sync$/,
 isSync = function(name){
   return reSync.test(name);
 },
-deSync = function(name){
-  return name.replace(reSync, '');
-},
 reAsync = /Async$/,
 isAsync = function(name){
   return reAsync.test(name);
 },
-deAsync = function(name){
-  return name.replace(reAsync, '');
+pureName = function(name){
+  return name.replace(reAsync, '').replace(reSync, '');
+},
+isStr = function(data){
+  return typeof data == 'string';
+},
+isNum = function(data){
+  return typeof data == 'number';
 },
 isFunc = function(data){
   return typeof data == 'function';
@@ -45,8 +48,7 @@ function TCError(self, func, args){
   Error.call(this);
   Error.captureStackTrace(this, arguments.callee);
   this.name = 'TCError';
-  if(typeof self.errmsg == 'function'
-     && typeof self.ecode == 'function'){
+  if(isFunc(self.errmsg) && isFunc(self.ecode)){
     this.message = self.errmsg(self.ecode());
   }
   if(func){
@@ -55,42 +57,182 @@ function TCError(self, func, args){
 };
 TCError.prototype.__proto__ = Error.prototype;
 
+var hooks = {
+  open: function(type, clas, orig){
+    return function(){
+      if(isStr(arguments[1])){
+        var i, m,
+        mode = 0,
+        opts = arguments[1].split(/[\|\+\.\-\/]/);
+        for(i = 0; i < opts.length; i++){
+          m = ('o' + opts[i]).toUpperCase();
+          if(m in clas){
+            mode |= clas[m];
+          }
+        }
+        arguments[1] = mode;
+      }/*else if(type == 'async' && (isFunc(arguments[1]) || )){
+        arguments.push(arguments[1]);
+        arguments[1] = clas.OREADER | clas.OCREAT;
+      }*/
+      return orig.apply(this, arguments);
+    };
+  },
+  addcond: function(type, clas, orig){
+    var cond = {
+      /* string */
+      '~=': clas.QCSTREQ,   /* equal */
+      '~@': clas.QCSTRINC,  /* include */
+      '@':  clas.QCSTRINC,
+      '~^': clas.QCSTRBW,   /* begin */
+      '^':  clas.QCSTRBW,
+      '~$': clas.QCSTREW,   /* end */
+      '$':  clas.QCSTREW,
+      '~&': clas.QCSTRAND,  /* all */
+      '&':  clas.QCSTRAND,
+      '~|': clas.QCSTROR,   /* one */
+      '|':  clas.QCSTROR,
+      '~*': clas.QCSTROREQ, /* in (tokens) */
+      '~?': clas.QCSTRRX,   /* regexp */
+      '~':  clas.QCSTRRX,
+      /* number */
+      '==': clas.QCNUMEQ,
+      '>':  clas.QCNUMGT,
+      '>=': clas.QCNUMGE,
+      '<':  clas.QCNUMLT,
+      '<=': clas.QCNUMLT,
+      '<>': clas.QCNUMBT,   /* between */
+      '><': clas.QCNUMBT,
+      '=*': clas.QCNUMOREQ, /* in (tokens) */
+      /* fulltext */
+      '?~': clas.QCFTSPH,   /* phrase */
+      '?&': clas.QCFTSAND,  /* all */
+      '?|': clas.QCFTSOR,   /* one */
+      '?*': clas.QCFTSEX,   /* compound */
+      /* flags */
+      '!': clas.QCNEGATE,   /* negate */
+      '%': clas.QCNOIDX,    /* no index */
+      '=': function(arg){ return isStr(arg) ? '~=' : '=='; },
+      '*': function(arg){ return isStr(arg) ? '~*' : '=*'; }
+    };
+    return clas == TC.TDBQRY ? function(){
+      if(isStr(arguments[1])){
+        var a = arguments[1],
+        c = 0, i, t;
+        for(; a; ){
+          for(i in cond){
+            a = a.split(i);
+            if(a.length > 1){
+              t = cond[i];
+              if(isFunc(t)){
+                t = t(arguments[2]);
+              }
+              c |= t;
+            }
+            a = a.join('');
+          }
+        }
+        arguments[1] = c;
+      }
+      orig.apply(this, arguments);
+      return this;
+    } : orig;
+  },
+  setorder: function(type, clas, orig){
+    var ord = {
+      '~>': clas.QOSTRASC,
+      '~<': clas.QOSTRDESC,
+      '>':  clas.QONUMASC,
+      '<':  clas.QONUMDESC
+    };
+    return clas == TC.TDBQRY ? function(){
+      if(isStr(arguments[1])){
+        var a = arguments[1],
+        o = 0, i;
+        for(; a; ){
+          for(i in ord){
+            if(a.search(i) > -1){
+              a = a.split(i).join('');
+              o |= ord[i];
+            }
+          }
+        }
+        arguments[1] = o;
+      }
+      orig.apply(this, arguments);
+      return this;
+    } : orig;
+  },
+  setlimit: function(type, clas, orig){
+    return clas == TC.TDBQRY ? function(){
+      orig.apply(this, arguments);
+      return this;
+    } : orig;
+  }
+};
+
+function Hook(name, type, clas, orig){
+  if(isFunc(hooks[name])){
+    return hooks[name](type, clas, orig);
+  }
+  return orig;
+}
+
+var OK = {};
+OK[TC.ESUCCESS] = 0;
+OK[TC.ENOREC] = 0;
+OK[TC.EKEEP] = 0;
+
 function Class(Native){
   var n,
   Proto = Native.prototype;
 
   for(n in Proto){
-    (function(name, data, name_){
+    (function(name, data, pure){
       if(isFunc(data)){
         if(isSync(name)){
-          Proto[name] = function(){
+          Proto[name] = Hook(pure, 'sync', Native, function(){
             var ret = data.apply(this, arguments);
-            if(typeof this.ecode == 'function' && this.ecode()){
+            if(isFunc(this.ecode) && !(this.ecode() in OK)){
               throw new TCError(this, name, arguments);
             }
             return ret;
-          };
-        }
-        if(isAsync(name)){
+          });
+        }else if(isAsync(name)){
           delete Proto[name];
-          name_ = deAsync(name);
-          Proto[name_] = function(){
+          Proto[pure] = Hook(pure, 'async', Native, function(){
             if(arguments.length > 0 && isFunc(arguments[arguments.length - 1])){
               var callback = arguments[arguments.length - 1],
               args = arguments,
               self = this;
               arguments[arguments.length - 1] = function(){
                 if(arguments[0]){
-                  arguments[0] = new TCError(self, name_, args);
+                  arguments[0] = new TCError(self, pure, args);
                 }
                 callback.apply(this, arguments);
               };
             }
             data.apply(this, arguments);
-          };
+          });
+        }else{
+          Proto[pure] = Hook(pure, '', Native, data);
         }
       }
-    })(n, Proto[n]);
+    })(n, Proto[n], pureName(n));
+  }
+  return Native;
+}
+
+function Alias(Native, tab){
+  var s, d, Proto = Native.prototype;
+  for(d in tab){
+    s = tab[d];
+    if(s in Proto){
+      Proto[d] = Proto[s];
+    }
+    if(s in Native){
+      Native[d] = Native[s];
+    }
   }
   return Native;
 }
@@ -105,4 +247,9 @@ exports = module.exports = {
 };
 
 exports.bdb.cur = Class(TC.BDBCUR);
-exports.tdb.qry = Class(TC.TDBQRY);
+
+exports.tdb.qry = Alias(Class(TC.TDBQRY), {
+  cond: 'addcond',
+  order: 'setorder',
+  limit: 'setlimit'
+});
